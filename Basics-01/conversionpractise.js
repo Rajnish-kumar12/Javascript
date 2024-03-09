a9a081aebe2dd708c1a9c8cea34961daa4c3e5fe
Rajnish kumar <rajnishkumardb07@gmail.com>
2024-03-09 16:32:43 +0530

    conversion-practise-file

---
+++ Basics-01/conversionpractise.js
@@ -0,0 +1,7 @@
+/* Let's try to connvert all type of primitive datatype which is in js into string
+___________________________________________________________________________________
+Number, undefined, null, boolean, symbol, Nan, string*/
+
+let score = 23;
+let scoreString = String(score);
+console.log(typeof scoreString);
