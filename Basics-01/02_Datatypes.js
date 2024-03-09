ede517c508ba1d11729821e92ab4b16a751db507
Rajnish kumar <rajnishkumardb07@gmail.com>
2024-03-09 13:27:11 +0530

    add-data-of-datatype-file

---
+++ Basics-01/02_Datatypes.js
@@ -0,0 +1,28 @@
+"use strict"  //treat all code of js as newer version
+
+// alert(3+3) we are using node js not browser engine
+
+console.log(3
+    +
+    3);    //code readability should be high
+
+
+console.log("Rajnish");
+
+//PRIMITIVE DATATYPES 
+
+// number  2 to power 53 range after that use bigint
+// string can we usable in single and double quotes
+// boolean--> true and false
+// null return type is object--->standalone value
+// eg) let state = null;
+
+// undefined  when you declare variable but not defined the value then it gives undefined
+// symbol---mostly used to find the uniquness
+
+// REFERENCE DATATYPES
+// object
+
+// * To check data type of value use typeof operator
+
+console.log(typeof null);
